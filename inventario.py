10b885bc2db1562d9336575c98c29ff29f1eb7c6
AvalonUC <avalonuc@gmail.com>
2024-12-10 00:27:11 -0500

    creacion de metodo editar

--- inventario.py
+++ inventario.py
@@ -10,3 +10,17 @@ class ProductoAlimenticio:
 class Inventario:
     def __init__(self):
         self.productos = []
+
+    def editar_producto(self, nombre, nuevo_nombre=None, nuevo_precio=None, nueva_cantidad=None):
+        # Editar un producto existente
+        for producto in self.productos:
+            if producto.nombre == nombre:
+                if nuevo_nombre:
+                    producto.nombre = nuevo_nombre
+                if nuevo_precio:
+                    producto.precio = nuevo_precio
+                if nueva_cantidad:
+                    producto.cantidad = nueva_cantidad
+                print(f"Producto {nombre} editado correctamente.")
+                return
+        print(f"Producto {nombre} no encontrado.")
