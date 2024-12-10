9f34772a869e34e7c391617a61efc88f098df34e
AvalonUC <avalonuc@gmail.com>
2024-12-10 00:23:21 -0500

    creacion de metodo listar

--- inventario.py
+++ inventario.py
@@ -10,3 +10,12 @@ class ProductoAlimenticio:
 class Inventario:
     def __init__(self):
         self.productos = []
+
+    def listar_productos(self):
+        # Mostrar todos los productos en el inventario
+        if not self.productos:
+            print("No hay productos en el inventario.")
+        else:
+            print("Productos en el inventario:")
+            for producto in self.productos:
+                print(producto)
